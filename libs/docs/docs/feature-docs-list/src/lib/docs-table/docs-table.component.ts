7c2881153357a8352afa4582b2ecf3bd9877dea0
Umar Baymran <1000slow@gmail.com>
2023-08-09 20:30:56 +0300

    feat(docs-list): UI component for displaying documents table

--- libs/docs/docs/feature-docs-list/src/lib/docs-table/docs-table.component.ts
+++ libs/docs/docs/feature-docs-list/src/lib/docs-table/docs-table.component.ts
@@ -1,9 +1,8 @@
 import {
   AfterViewInit,
   ChangeDetectionStrategy,
-  Component, inject,
-  Input,
-  OnChanges, OnInit, ViewChild
+  Component, Input,
+  OnChanges, ViewChild
 } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatTableDataSource, MatTableModule} from "@angular/material/table";
@@ -11,7 +10,7 @@ import {MatCheckboxModule} from "@angular/material/checkbox";
 import {MatPaginator, MatPaginatorModule} from "@angular/material/paginator";
 import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
-import {MatSort, MatSortModule, Sort} from "@angular/material/sort";
+import {MatSort, MatSortModule} from "@angular/material/sort";
 import {DocsListVm} from "../docs-list-vm";
 import {DocumentVm} from "../../../../document-vm";
 import {MatIconModule} from "@angular/material/icon";
@@ -19,7 +18,7 @@ import {CapitalizeFirstLetterPipe} from "@core/utils";
 import {CdkTableModule} from "@angular/cdk/table";
 import {MatMenuModule} from "@angular/material/menu";
 import {MatButtonModule} from "@angular/material/button";
-import {LiveAnnouncer} from "@angular/cdk/a11y";
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'docs-table',
@@ -27,16 +26,18 @@ import {LiveAnnouncer} from "@angular/cdk/a11y";
   imports: [CommonModule, MatTableModule, MatCheckboxModule, MatPaginatorModule, MatInputModule, MatSelectModule, MatSortModule, MatIconModule, CapitalizeFirstLetterPipe, CdkTableModule, MatMenuModule, MatButtonModule],
   templateUrl: './docs-table.component.html',
   styleUrls: ['./docs-table.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DocsTableComponent implements OnChanges, AfterViewInit {
   @Input({required: true}) vm!: DocsListVm;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
+
   public dataSource = new MatTableDataSource<DocumentVm>();
-  private readonly liveAnnouncer = inject(LiveAnnouncer)
+  public showArchival = true;
 
   displayedColumns: string[] = [
+    'main',
     'type',
     'series',
     'number',
@@ -45,15 +46,25 @@ export class DocsTableComponent implements OnChanges, AfterViewInit {
 
   // "id" | "organization" | "type" | "series" | "number" | "dateOfIssue" | "main" | "archival"
 
+  ngOnChanges() {
+    if (this.vm.docs) {
+      this.patchDataSource()
+    }
+  }
+
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
 
-  ngOnChanges() {
-    if (this.vm.docs) {
-      this.dataSource.data = [...this.vm.docs];
-      console.log('on changes')
-    }
+  private patchDataSource() {
+    this.dataSource.data = this.showArchival
+      ? [...this.vm.docs]
+      : this.vm.docs.filter(item => !item.archival);
+  }
+
+  public toggleArchival() {
+    this.showArchival = !this.showArchival;
+    this.patchDataSource()
   }
 }
